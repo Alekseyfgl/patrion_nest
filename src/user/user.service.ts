4c50f8560f4f33ff8123a66e83e1236278b15c95
aleksey <aleksey.fgl@gmail.com>
2024-02-28 18:27:52 +0000

    add log for env

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { RegistrationUserDto } from './interfeces/input';
 import { UserCommandRepository } from './repository/user.command.repository';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
-    constructor(protected userCommandRepository: UserCommandRepository) {}
+    constructor(
+        protected userCommandRepository: UserCommandRepository,
+        private configService: ConfigService,
+    ) {}
 
     async create(dto: RegistrationUserDto) {
+        this.logEnv();
         return this.userCommandRepository.create(dto);
+    }
+
+    logEnv() {
+        const saltRounds = this.configService.get<number>('SALT_ROUNDS', 10);
+        console.log(saltRounds);
     }
 }
