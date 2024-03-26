fd2283b14c3e2ea55f80293fd82635e6f6efcd43
aleksey <aleksey.fgl@gmail.com>
2024-03-26 19:23:26 +0000

    fixed interfaces

--- src/common/http-exceptions-service/exception.interface.ts
+++ src/common/http-exceptions-service/exception.interface.ts
@@ -1,3 +1,5 @@
+import { HttpExceptionMessagesConst } from '../constans/http-exception-messages.const';
+
 export interface ICustomErrors {
     errorsMessages: IErrorMessage[];
 }
@@ -5,4 +7,11 @@ export interface ICustomErrors {
 export interface IErrorMessage {
     message: string;
     field: string;
+}
+
+export interface IException {
+    badRequestException(message: HttpExceptionMessagesConst, field: string): void;
+    internalServerErrorException(message: HttpExceptionMessagesConst, field: string): void;
+    forbiddenException(message: HttpExceptionMessagesConst, field: string): void;
+    unauthorizedException(message: HttpExceptionMessagesConst, field: string): void;
 }
--- src/common/http-exceptions-service/exeption.service.ts
+++ src/common/http-exceptions-service/exeption.service.ts
@@ -1,12 +1,6 @@
 import { BadRequestException, ForbiddenException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { HttpExceptionMessagesConst } from '../constans/http-exception-messages.const';
-
-export interface IException {
-    badRequestException(message: HttpExceptionMessagesConst, field: string): void;
-    internalServerErrorException(message: HttpExceptionMessagesConst, field: string): void;
-    forbiddenException(message: HttpExceptionMessagesConst, field: string): void;
-    unauthorizedException(message: HttpExceptionMessagesConst, field: string): void;
-}
+import { IException } from './exception.interface';
 
 @Injectable()
 export class ExceptionsService implements IException {
